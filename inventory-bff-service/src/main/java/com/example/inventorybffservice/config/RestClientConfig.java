d50ec6e643aa30bd127b4acb371d7ed1e3962344
RashmiAbhay <rashmiabhaykrishna@gmail.com>
2026-05-12 22:31:48 +0530

    Committing by adding tracing dependency.

--- inventory-bff-service/src/main/java/com/example/inventorybffservice/config/RestClientConfig.java
+++ inventory-bff-service/src/main/java/com/example/inventorybffservice/config/RestClientConfig.java
@@ -1,15 +1,22 @@
 package com.example.inventorybffservice.config;
 
-
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
 
+    private final ObservationRegistry observationRegistry;
+
     @Bean
-    public RestClient restClient() {
-        return RestClient.create();
+    RestClient restClient() {
+
+        return RestClient.builder()
+                .observationRegistry(observationRegistry)
+                .build();
     }
 }
